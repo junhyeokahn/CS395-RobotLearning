cf01e1d4841bbeba557d39e6d28aea8f0a6d9ef8
junhyeokahn <junhyeokahn91@gmail.com>
2017-10-10 17:20:57 -0500

    hw2

---
+++ hw2/np_prac.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+##
+a = np.array([ [ [1,2,1,2],[3,4,3,4] ], [ [5,6,5,6],[7,8,7,8] ], [ [9,10,9,10],[11,12,11,12] ] ])
+e = np.array([ [ [1],[2] ], [ [1],[2] ], [ [1],[2] ] ])
+b = np.array([ [[2]], [[1]], [[3]] ])
+c = np.array([ [ [1],[2] ] ])
+d = np.array([ [ [1, 2] ] ])
+
+##
+# def add(x):
+    # return x + 1
+
+##
+a = [1,2,3]
+for i in a:
+    print(i)
+import ipdb
+ipdb.set_trace()
+
+##
+# a = np.array([[1,2,3],[4,5,6]])
+# b = np.array([2, 3])
+
+##
+
